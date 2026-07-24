a785ee2bd0b5303101f2df66c78ced217123f69e
Asim Ibrahimli <asim.ibrahimov02@gmail.com>
2026-07-24 17:31:06 +0900

    auth: some changes for local development about sessions

--- config/session.php
+++ config/session.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'connection' => env('SESSION_CONNECTION', 'sessions'),
+    'connection' => env('SESSION_CONNECTION', 'null'),
 
     /*
     |--------------------------------------------------------------------------
